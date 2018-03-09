ef246d13f757cfa9341d94cb06bdf5a308bbd663
tshevchenko271285 <tshevchenko@ro.ru>
2018-03-09 10:34:26 +0200

    Lesson 11

--- models/TestForm.php
+++ models/TestForm.php
@@ -9,6 +9,42 @@ class TestForm extends Model {
 	public $email;
 	public $text;
 
+	public function attributeLabels(){
+		return [
+			'name' => 'Имя',
+			'email' => 'E-mail',
+			'text' => 'Текст сообщения'
+		];
+	}
 
+	public function rules() {
+		return [
+			// Правила валидации
+			//[ ['name', 'email'], 'required', 'message' => 'Текст сообщения' ],
+
+			// К перечисленным полям применяется правило "Обязательны к заполнению"
+			[ ['name', 'email'], 'required' ],
+
+			// Указанное поле проверяется выбранным валидатором
+			[ 'email', 'email' ],
+
+			// Указанное поле проверяется на длину строки length может быть 2 или [2, 4]
+			//[ 'name', 'string', 'length' => [2, 2], 'tooShort' => 'Минимальное колличество 2 символа', 'tooLong' => 'Максимальная длина сообщения 2 символа' ],
+			[ 'name', 'string', 'min' => 2, 'tooShort' => 'Минимальное колличество 2 символа' ],
+			[ 'name', 'string', 'max' => 2, 'tooLong' => 'Максимальная длина Вашего имени 2 символа' ],
+
+			// Обрезает пробелы
+			['text', 'trim'],
+
+			// Определение собственного валидатора вторым параметром указываем имя метода(не работает на клиенте)
+			['name', 'myRule'],
+		];
+	}
+	//Определение собственного валидатора
+	public function myRule($attr){
+		if( !in_array( $this->attr, ['hello', 'world'] ) ) {
+			$this->addError($this->attr, 'Wrong!');
+		}
+	}
 
 }
--- views/Post/test.php
+++ views/Post/test.php
@@ -7,8 +7,8 @@
 <h1>Test Action</h1>
 
 <?php $form = ActiveForm::begin( ['options' => ['id' => 'testForm' ] ] ); ?>
-<?= $form->field($model, 'name')->input('name')->label('Имя') ?>	
-<?= $form->field($model, 'email')->input('email')->label('E-mail') ?>	
-<?= $form->field($model, 'text')->textarea([ 'rows' => 5 ])->label('Текст Сообщения') ?>
+<?= $form->field($model, 'name')->input('name') ?>	
+<?= $form->field($model, 'email')->input('email') ?>	
+<?= $form->field($model, 'text')->textarea([ 'rows' => 5 ]) ?>
 <?= Html::submitButton('Отправить', ['class'=> 'btn btn-success']) ?>
 <?php ActiveForm::end(); ?>
