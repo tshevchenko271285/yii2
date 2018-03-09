e8e17ab62d1b77d7396b08134a70467e1b70a048
tshevchenko271285 <tshevchenko@ro.ru>
2018-03-09 09:46:58 +0200

    Lesson 10

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -3,34 +3,42 @@
 namespace app\controllers;
 
 use Yii;
+use app\models\TestForm;
 
 class PostController extends AppController
 {
 	public $layout = 'basic'; // Set Layout for the Controller
 
+/*	// Отключение CSRF защиты
 	public function beforeAction($action){
 		// При переходе на Action Index
 		if ( $action->id == 'index' ) {
 			// Oтключаем Csrf защиту
 			$this->enableCsrfValidation = false;
 		}
+		// Вызываем радительский метод и передаем в него отредактированный $action
 		return parent::beforeAction($action);
 	}
-
+*/
 	public function actionIndex(){
 
 		if( Yii::$app->request->isAjax ) { // Если пришел AJAX запрос
 			return json_encode( Yii::$app->request->post() ); // Вернём глобальный массив $_POST
 		}
 
+		$model = new TestForm();
+		$this->view->title = 'Все статьи'; // Определение Title
+		return $this->render( 'test', compact( 'model' ) );
+	/*	// Отправка Данных в View
 		$arr = [1,2,3];
 		return $this->render('test', compact('arr') );
 		return $this->render('test', ['arr' => $arr, ] );
+	*/
 	}
 
 	public function actionShow(){
 
-		//$this->layout = 'basic'; // Set Layout for the Action
+		//$this->layout = 'basic'; // Установка layaut для этого action
 		
 		$this->view->title = 'Одна статья!'; // Определение Title
 
--- views/Post/test.php
+++ views/Post/test.php
@@ -1,5 +1,14 @@
-<h1>Test Action</h1>
-<?php //var_dump($arr) ?>
 <?php 
-	debug($arr)
- ?>
+	use yii\widgets\ActiveForm;
+	//use yii\widgets\ActiveField;
+	use yii\helpers\Html;
+
+?>
+<h1>Test Action</h1>
+
+<?php $form = ActiveForm::begin( ['options' => ['id' => 'testForm' ] ] ); ?>
+<?= $form->field($model, 'name')->input('name')->label('Имя') ?>	
+<?= $form->field($model, 'email')->input('email')->label('E-mail') ?>	
+<?= $form->field($model, 'text')->textarea([ 'rows' => 5 ])->label('Текст Сообщения') ?>
+<?= Html::submitButton('Отправить', ['class'=> 'btn btn-success']) ?>
+<?php ActiveForm::end(); ?>
