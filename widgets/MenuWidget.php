716956a0d2fb7903d8251a7d31e260ed8f4b58c8
tshevchenko271285 <tshevchenko@ro.ru>
2018-03-21 10:39:07 +0200

    'Lesson-5'

--- widgets/MenuWidget.php
+++ widgets/MenuWidget.php
@@ -3,53 +3,61 @@ namespace app\widgets;
 
 use yii\base\Widget;
 use app\models\Category;
+use Yii;
 
 class MenuWidget extends Widget {
 
-    public $tpl;
-    public $data;
-    public $tree;
-    public $menuHtml;
+	public $tpl;
+	public $data;
+	public $tree;
+	public $menuHtml;
 
-    public function init(){
-        parent::init();
-        if( $this->tpl === null ) {
-            $this->tpl = 'menu';
-        }
-        $this->tpl .= '.php';
-    }
+	public function init(){
+		parent::init();
+		if( $this->tpl === null ) {
+			$this->tpl = 'menu';
+		}
+		$this->tpl .= '.php';
+	}
 
-    public function run(){
-        // indexBy() индексы возващяемого массива будут присвоены согласно указанному полю.
-        $this->data = Category::find()->indexBy('id')->asArray()->all();
-        $this->tree = $this->getTree();
-        $this->menuHtml = $this->getMenuHtml($this->tree);
-        //debug($this->tree);
-        return $this->menuHtml;
-    }
+	public function run(){
 
-    protected function getTree(){
-        $tree = [];
-        foreach ($this->data as $id=>&$node) {
-            if ( !$node['parent_id'] ) {
-                $tree[$id] = &$node;
-            } else {
-                $this->data[$node['parent_id']]['childs'][$node['id']] = &$node;
-            }
-        }
-        return $tree;
-    }
+		// Получаем данные из Кэша
+		$menu = Yii::$app->cache->get('menu');
+		if($menu) return $menu;
 
-    protected function getMenuHtml($three){
-        $str = '';
-        foreach($three as $category) {
-            $str .= $this->catToTemplate($category);
-        }
-        return $str;
-    }
-    protected function catToTemplate($category){
-        ob_start();
-        include __DIR__ . '/menu_tpl/' . $this->tpl;
-        return ob_get_clean();
-    }
+		// indexBy() индексы возващяемого массива будут присвоены согласно указанному полю.
+		$this->data = Category::find()->indexBy('id')->asArray()->all();
+		$this->tree = $this->getTree();
+		$this->menuHtml = $this->getMenuHtml($this->tree);
+
+		// Записываем данные в Кэш (ключ, данные, время)
+		Yii::$app->cache->set('menu', $this->menuHtml, 60);
+		return $this->menuHtml;
+	}
+
+	protected function getTree(){
+		$tree = [];
+		foreach ($this->data as $id=>&$node) {
+			if ( !$node['parent_id'] ) {
+				$tree[$id] = &$node;
+			} else {
+				$this->data[$node['parent_id']]['childs'][$node['id']] = &$node;
+			}
+		}
+		return $tree;
+	}
+
+	protected function getMenuHtml($three){
+		$str = '';
+		foreach($three as $category) {
+			$str .= $this->catToTemplate($category);
+		}
+		return $str;
+	}
+	protected function catToTemplate($category){
+		ob_start();
+		include __DIR__ . '/menu_tpl/' . $this->tpl;
+		return ob_get_clean();
+	}
 }
