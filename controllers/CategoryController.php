3eb2b943d4d2f4570c15ee164ad480514145efed
tshevchenko271285 <tshevchenko@ro.ru>
2018-03-22 11:01:34 +0200

    'Lesson-7'

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -9,6 +9,7 @@
 namespace app\controllers;
 use app\models\Category;
 use app\models\Product;
+use Yii;
 
 
 
@@ -17,5 +18,11 @@ class CategoryController extends AppController
     public function actionIndex(){
         $hits = Product::find()->where([ 'hit' => '1' ])->limit(6)->all();
         return $this->render('index', compact( 'hits' ));
+    }
+
+    public function actionView(){
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where( ['category_id' => $id] )->all();
+        return $this->render('view', compact('products'));
     }
 }
