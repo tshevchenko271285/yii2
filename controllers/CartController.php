f299123d8a5f4c8d908425c42b3b0d27c6befcc7
tshevchenko <tshevchenko@ro.ru>
2018-04-04 11:48:39 +0300

    'Lesson-19'

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -2,6 +2,9 @@
 
 namespace app\controllers;
 
+
+use app\models\Order;
+use app\models\OrderItems;
 use app\models\Product;
 use app\models\Cart;
 use Yii;
@@ -10,22 +13,18 @@ use Yii;
 class CartController extends AppController {
 
     public function actionAdd() {
-
         $id = Yii::$app->request->get('id');
         $qty = (INT) Yii::$app->request->get('qty');
         $qty = !$qty ? 1 : $qty;
-
         $product = Product::findOne($id);
         if( empty( $product ) ) echo false;
         $session = Yii::$app->session;
         $session->open();
         $cart = new Cart();
         $cart->addToCart($product, $qty);
-
         /*if( !Yii::$app->request->isAjax ){  !!! Не работает редирект!
             return $this->redirect(Yii::$app->request->referrer);
         }*/
-
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
     }
@@ -36,65 +35,57 @@ class CartController extends AppController {
         $session->remove('cart');
         $session->remove('cart.qty');
         $session->remove('cart.sum');
-
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
     }
 
     public function actionDelItem() {
-
         $id = Yii::$app->request->get('id');
-
         $session = Yii::$app->session;
         $session->open();
-
         $cart = new Cart();
         $cart->recalc($id);
-
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
     }
 
     public function actionIncItem(){
         $id = Yii::$app->request->get('id');
-
         $session = Yii::$app->session;
         $session->open();
-
         $cart = new Cart();
         $cart->incProduct($id);
-
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
     }
 
     public function actionDecItem(){
         $id = Yii::$app->request->get('id');
-
         $session = Yii::$app->session;
         $session->open();
-
         $cart = new Cart();
         $cart->decProduct($id);
-
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
-
     }
 
     public function actionShow() {
-
         $id = Yii::$app->request->get('id');
-
         $session = Yii::$app->session;
         $session->open();
-
         $this->layout = false;
         return $this->render('cart-modal', compact('session'));
     }
 
     public function actionView(){
-        return $this->render('view');
+        $session = Yii::$app->session;
+        $session->open();
+        $this->setMeta('Корзина');
+        $order = new Order();
+        if( $order->load( Yii::$app->request->post() ) ) {
+            debug( Yii::$app->request->post() );
+        }
+        return $this->render('view', compact('session', 'order'));
     }
 
 }
--- views/Cart/view.php
+++ views/Cart/view.php
@@ -1,1 +1,91 @@
-Корзина
+<?
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+?>
+
+<div class="container">
+    <section id="cart_items">
+        <div class="col-md-12">
+        <?php if( !empty( $session['cart'] ) ) : ?>
+        <!--<div class="container">-->
+        <div class="breadcrumbs">
+            <ol class="breadcrumb">
+                <li><a href="#">Home</a></li>
+                <li class="active">Shopping Cart</li>
+            </ol>
+        </div>
+        <div class="table-responsive cart_info">
+            <table class="table table-condensed">
+                <thead>
+                <tr class="cart_menu">
+                    <td class="image">Фото</td>
+                    <td class="description"></td>
+                    <td class="price">Цена</td>
+                    <td class="quantity">Количество</td>
+                    <td class="total">Всего</td>
+                    <td></td>
+                </tr>
+                </thead>
+                <tbody>
+                <? //debug($session['cart']) ?>
+                <?php foreach ( $session['cart'] as $key => $item ) : ?>
+                    <?php if( !$item['name'] ) continue; ?>
+                    <tr>
+                        <td class="cart_product">
+                            <a href="<?= Url::to(['product/view', 'id' => $key])?>">
+                                <? $options = ['style' => ['width' => '150px'], 'alt' =>  $item['name'], ]; ?>
+                                <?= Html::img("@web/images/product/{$item['img']}", $options); ?>
+                            </a>
+                        </td>
+                        <td class="cart_description">
+                            <h4><a href="<?= Url::to(['product/view', 'id' => $key ])?>"><?= $item['name'] ?></a></h4>
+                            <p>Web ID: <?= $key ?></p>
+                        </td>
+                        <td class="cart_price">
+                            <p>$<?= $item['price'] ?></p>
+                        </td>
+                        <td class="cart_quantity">
+                            <div class="cart_quantity_button">
+                                <a data-id="<?= $key ?>" class="cart_quantity_up" href=""> + </a>
+                                <input class="cart_quantity_input" type="text" name="quantity" value="<?= $item['qty'] ?>" autocomplete="off" size="2">
+                                <a data-id="<?= $key ?>" class="cart_quantity_down" href=""> - </a>
+                            </div>
+                        </td>
+                        <td class="cart_total">
+                            <p class="cart_total_price">$<?= $item['price'] * $item['qty'] ?></p>
+                        </td>
+                        <td class="cart_delete">
+                            <a class="cart_quantity_delete" data-id="<?= $key ?>" href=""><i class="fa fa-times"></i></a>
+                        </td>
+                    </tr>
+                <?php endforeach;?>
+                <tr>
+                    <td></td>
+                    <td colspan="3">Итого: </td>
+                    <td><?= $session['cart.qty']?></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td colspan="3">На сумму: </td>
+                    <td>$<?= $session['cart.sum'] ?></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        </div>
+        <hr>
+        <div class="col-md-6">
+            <?php $form = ActiveForm::begin();?>
+                <?= $form->field($order, 'name')?>
+                <?= $form->field($order, 'email')?>
+                <?= $form->field($order, 'phone')?>
+                <?= $form->field($order, 'address')?>
+                <?= Html::submitButton('Заказать', ['class'=>'btn btn-success'])?>
+            <?php ActiveForm::end();?>
+        </div>
+    <?php else : ?>
+    <h3>Корзина пуста</h3>
+    <?php endif;?>
+    </section><!--/#cart_items-->
+</div>
